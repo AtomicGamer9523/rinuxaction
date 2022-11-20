91d682bb9823aa483e0819cabe5201454be15069
Hypernite <drunkenmonkey4987@gmail.com>
2022-11-19 21:28:35 -0500

    Minor Fixes

--- src/python/python.ts
+++ src/python/python.ts
@@ -2,9 +2,8 @@ import * as core from '@actions/core';
 import * as finder from './find-python';
 import * as path from 'path';
 import * as os from 'os';
-import fs from 'fs';
 import { getCacheDistributor } from './cache-distributions/cache-factory';
-import { isCacheFeatureAvailable, logWarning, IS_MAC } from '../utils/utils';
+import { isCacheFeatureAvailable, IS_MAC } from '../utils/utils';
 
 function isPyPyVersion(versionSpec: string) {
     return versionSpec.startsWith('pypy');
@@ -21,45 +20,45 @@ async function cacheDependencies(cache: string, pythonVersion: string) {
     await cacheDistributor.restoreCache();
 }
 
-function resolveVersionInput(): string {
-    let version = core.getInput('python-version');
-    let versionFile = core.getInput('python-version-file');
+// function resolveVersionInput(): string {
+//     let version = '3.10';
+//     let versionFile = core.getInput('python-version-file');
 
-    if (version && versionFile) {
-        core.warning(
-            'Both python-version and python-version-file inputs are specified, only python-version will be used.'
-        );
-    }
+//     if (version && versionFile) {
+//         core.warning(
+//             'Both python-version and python-version-file inputs are specified, only python-version will be used.'
+//         );
+//     }
 
-    if (version) {
-        return version;
-    }
+//     if (version) {
+//         return version;
+//     }
 
-    if (versionFile) {
-        if (!fs.existsSync(versionFile)) {
-            throw new Error(
-                `The specified python version file at: ${versionFile} doesn't exist.`
-            );
-        }
-        version = fs.readFileSync(versionFile, 'utf8');
-        core.info(`Resolved ${versionFile} as ${version}`);
-        return version;
-    }
+//     if (versionFile) {
+//         if (!fs.existsSync(versionFile)) {
+//             throw new Error(
+//                 `The specified python version file at: ${versionFile} doesn't exist.`
+//             );
+//         }
+//         version = fs.readFileSync(versionFile, 'utf8');
+//         core.info(`Resolved ${versionFile} as ${version}`);
+//         return version;
+//     }
 
-    logWarning(
-        "Neither 'python-version' nor 'python-version-file' inputs were supplied. Attempting to find '.python-version' file."
-    );
-    versionFile = '.python-version';
-    if (fs.existsSync(versionFile)) {
-        version = fs.readFileSync(versionFile, 'utf8');
-        core.info(`Resolved ${versionFile} as ${version}`);
-        return version;
-    }
+//     logWarning(
+//         "Neither 'python-version' nor 'python-version-file' inputs were supplied. Attempting to find '.python-version' file."
+//     );
+//     versionFile = '.python-version';
+//     if (fs.existsSync(versionFile)) {
+//         version = fs.readFileSync(versionFile, 'utf8');
+//         core.info(`Resolved ${versionFile} as ${version}`);
+//         return version;
+//     }
 
-    logWarning(`${versionFile} doesn't exist.`);
+//     logWarning(`${versionFile} doesn't exist.`);
 
-    return version;
-}
+//     return version;
+// }
 
 export = async function run(): Promise<void> {
     if (IS_MAC) {
@@ -74,21 +73,18 @@ export = async function run(): Promise<void> {
         `Python is expected to be installed into ${process.env['RUNNER_TOOL_CACHE']}`
     );
     try {
-        const version = resolveVersionInput();
-        const checkLatest = core.getBooleanInput('check-latest');
-
+        const version = "3.10";
         if (version) {
             let pythonVersion: string;
-            const arch: string = core.getInput('architecture') || os.arch();
-            const updateEnvironment = core.getBooleanInput('update-environment');
+            const arch: string = os.arch();
             if (isPyPyVersion(version)) {
                 core.error("Somehow we're trying to install PyPy");
             } else {
                 const installed = await finder.useCpythonVersion(
                     version,
                     arch,
-                    updateEnvironment,
-                    checkLatest
+                    true,
+                    false
                 );
                 pythonVersion = installed.version;
                 core.info(`Successfully set up ${installed.impl} (${pythonVersion})`);
