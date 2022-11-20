92575d70b75a3d4747fbbac83f67dde8033e3a0e
Hypernite <drunkenmonkey4987@gmail.com>
2022-11-20 09:36:23 -0500

    Added auto release

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -74189,6 +74189,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(8087));
 const toolchain_1 = __importDefault(__nccwpck_require__(434));
+const release_1 = __importDefault(__nccwpck_require__(4041));
 const utils_1 = __nccwpck_require__(9291);
 const python_1 = __importDefault(__nccwpck_require__(8539));
 const cargo_1 = __importDefault(__nccwpck_require__(6910));
@@ -74216,6 +74217,8 @@ async function main() {
         core.debug("Building bootloader...");
         await (0, utils_1.execCmd)("cargo", ["bootimage", "--release"]);
         core.debug("Bootloader built.");
+        core.debug("releasing package...");
+        await (0, release_1.default)("RustOS", "RustOS release", false);
     }
     catch (error) {
         core.setFailed(error.message);
@@ -74925,6 +74928,60 @@ module.exports = async function run() {
     catch (err) {
         core.setFailed(err.message);
     }
+};
+
+
+/***/ }),
+
+/***/ 4041:
+/***/ (function(module, __unused_webpack_exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+const ghub = __importStar(__nccwpck_require__(5376));
+module.exports = async function release(name, body, release) {
+    const github = ghub.getOctokit(process.env.GITHUB_TOKEN || '');
+    const { owner: currentOwner, repo: currentRepo } = ghub.context.repo;
+    const tag = "";
+    const releaseName = name;
+    const draft = true;
+    const prerelease = release;
+    const commitish = ghub.context.sha;
+    const owner = currentOwner;
+    const repo = currentRepo;
+    await github.repos.createRelease({
+        owner,
+        repo,
+        tag_name: tag,
+        name: releaseName,
+        body: body,
+        draft,
+        prerelease,
+        target_commitish: commitish
+    });
 };
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from "@actions/core";
 
 import toolchain from "./toolchain/toolchain";
-// import release from "./release/release";
+import release from "./release/release";
 import { execCmd } from "./utils/utils";
 import python from "./python/python";
 import cargo from "./cargo/cargo";
@@ -37,8 +37,8 @@ async function main(): Promise<void> {
             ["bootimage","--release"]
         );
         core.debug("Bootloader built.");
-        // core.debug("releasing package...");
-        // await release("RustOS", "RustOS release", false);
+        core.debug("releasing package...");
+        await release("RustOS", "RustOS release", false);
     } catch (error) {
         core.setFailed((<Error>error).message);
         return;
